c0606503e17c6d2c458ed8dc54a8a5a3e4b75a4f
Ichinga Samuel <ichingasamuel@gmail.com>
2024-01-18 03:39:36 +0100

    3.15v

---
+++ utils/update_records.py
@@ -0,0 +1,12 @@
+import asyncio
+from aiomql import Records, Account
+
+
+async def update_records():
+    """Update all records"""
+    async with Account() as acc:
+        rec = Records()
+        await rec.update_records()
+
+
+asyncio.run(update_records())
