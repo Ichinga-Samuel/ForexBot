743533114fb7e565e838ec1cc390b6c78c3a8b0d
Ichinga Samuel <ichingasamuel@gmail.com>
2024-03-26 04:45:45 +0100

    fix trailing stops

--- src/strategies/finger_trap.py
+++ src/strategies/finger_trap.py
@@ -23,7 +23,7 @@ class FingerTrap(Strategy):
     tracker: Tracker
 
     parameters = {"fast_ema": 8, "slow_ema": 20, "etf": TimeFrame.M15, 'closer': ema_closer,
-                  "ttf": TimeFrame.H1, "entry_ema": 5, "tcc": 720, "ecc": 4320,
+                  "ttf": TimeFrame.H4, "entry_ema": 5, "tcc": 720, "ecc": 1440,
                   'profit_levels': [0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4,
                                     0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0.025]}  # 1
 
