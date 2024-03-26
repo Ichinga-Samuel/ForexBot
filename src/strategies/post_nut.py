60dad6a19dbf706ae399745912d0c3c444c7f10e
Ichinga Samuel <ichingasamuel@gmail.com>
2024-03-26 04:50:25 +0100

    fix trailing stops

--- src/strategies/post_nut.py
+++ src/strategies/post_nut.py
@@ -12,7 +12,7 @@ from ..utils.patterns import is_half_bearish_fractal, is_half_bullish_fractal
 from ..traders.sp_trader import SPTrader
 from ..utils.tracker import Tracker
 from ..utils.ram import RAM
-from ..closers import ema_rsi_closer
+from ..closers import ema_closer
 
 logger = getLogger(__name__)
 
@@ -32,7 +32,7 @@ class PostNut(Strategy):
     trend: int
     interval: int = 180
     parameters = {"ttf": TimeFrame.M15, "etf": TimeFrame.M15, "tcc": 720, "ecc": 4320, "first_sma": 5, "second_sma": 9,
-                  "mfi_length": 14, "third_sma": 2, 'fourth_sma': 15, 'interval': 180, 'closer': ema_rsi_closer,
+                  "mfi_length": 14, "third_sma": 2, 'fourth_sma': 15, 'interval': 180, 'closer': ema_closer,
                   'cap': 5, 'ntr': True}
 
     def __init__(self, *, symbol: Symbol, trader: Trader = None, sessions: Sessions = None, name: str = 'PostNut'):
