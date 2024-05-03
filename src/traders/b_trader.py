fea0e189ddcffe8ee5830870934806ee3de35f7d
Ichinga Samuel <ichingasamuel@gmail.com>
2024-05-03 04:57:49 +0100

    ff

--- src/traders/b_trader.py
+++ src/traders/b_trader.py
@@ -29,12 +29,13 @@ class BTrader(BaseTrader):
 
     def save_profit(self, result: OrderSendResult, profit):
         try:
-            winning = {'current_profit': profit, 'trail_start': 4.5, 'trail': 1.5, 'trailing': False,
-                       'extend_start': 0.75, 'start_trailing': True, 'extend_by': 0.5, 'use_trails': True,
-                       'trails': {4.0: 2.5, 6: 4.5, 8: 6.0, 10: 8}, 'last_profit': 0} | self.trail_profits
+            winning = ({'current_profit': profit, 'trail_start': 4.5, 'trail': 1.5, 'trailing': False,
+                        'extend_start': 0.75, 'start_trailing': True, 'extend_by': 0.5, 'use_trails': True,
+                        'trails': {4.5: 3.5, 5: 3.5, 6: 4.5, 7: 6.5, 8: 6.0, 10: 8, 9: 7}, 'last_profit': 0}
+                       | self.trail_profits)
 
             losing = {'trail_start': 0.75, 'sl_limit': 5, 'trail': 0.75, 'trailing': True,
-                      'last_profit': 0, 'hedge_point': -2, 'cut_off': -1,
+                      'last_profit': 0, 'hedge_point': -3, 'cut_off': -1,
                       'hedge_cutoff': 0} | self.trail_loss
             fixed_closer = {'close': False, 'cut_off': -1} | self.fixed_closer
             self.config.state['winning'][result.order] = winning
