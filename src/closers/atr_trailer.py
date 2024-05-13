942bf8bc1194e4796388b1d688764f24955b7565
Ichinga Samuel <46368656+Ichinga-Samuel@users.noreply.github.com>
2024-05-13 14:55:57 +0100

    update atr_trailer.py

--- src/closers/atr_trailer.py
+++ src/closers/atr_trailer.py
@@ -7,7 +7,7 @@ from ..utils.sym_utils import calc_profit
 logger = getLogger(__name__)
 
 
-async def trailing_sl(*, position: TradePosition):
+async def atr_trailer(*, position: TradePosition):
     try:
         config = Config()
         order = config.state['atr_trailer'][position.ticket]
@@ -16,7 +16,7 @@ async def trailing_sl(*, position: TradePosition):
             return
         await modify_stops(position=position, order=order)
     except Exception as err:
-        logger.error(f"{err} in modify_stop for {position.symbol}:{position.ticket}")
+        logger.error(f"{err} in atr_trailer for {position.symbol}:{position.ticket}")
 
 
 async def modify_stops(*, position: TradePosition, order: dict, extra: float = 0.0, tries: int = 4):
@@ -27,10 +27,11 @@ async def modify_stops(*, position: TradePosition, order: dict, extra: float = 0
         await symbol.init()
         etf = params['etf']
         ecc = params['ecc']
-        atr_multiplier = params['atr_multiplier']
+        atr = params['atr']
+        atr_factor = params['atr_factor']
         candles = await symbol.copy_rates_from_pos(timeframe=etf, count=ecc)
         candles.ta.atr(append=True)
-        candles.rename(inplace=True, **{'ATRr_14': 'atr'})
+        candles.rename(inplace=True, **{f'ATRr_{atr}': 'atr'})
         current = candles[-1]
         tick = await symbol.info_tick()
         expected_profit = order['expected_profit']
@@ -40,26 +41,32 @@ async def modify_stops(*, position: TradePosition, order: dict, extra: float = 0
         min_value = round(min_points * symbol.point, symbol.digits)
 
         if position.type == OrderType.BUY:
-            atr_value = current.atr * atr_multiplier
+            atr_value = current.atr * atr_factor
             if atr_value < min_value:
                 atr_value = min_value
                 logger.warning(f"ATR value adjusted to {atr_value} for {symbol}")
             sl = tick.ask - atr_value
-            assert sl > position.sl, f"current_sl={position.sl} >than new sl={sl} in long"
+            assert sl > max(position.sl, position.price_open), f"current_sl={position.sl} >than new sl={sl} in long"
             if (position.profit / expected_profit) > extend_profit:
-                tp = position.tp + current.atr * atr_multiplier
+                profit_extension = 1 - extend_profit
+                full_points = abs(position.price_open - position.tp) / symbol.point
+                profit_extension = round(profit_extension * full_points * symbol.point, symbol.digits)
+                tp = position.tp + profit_extension
                 change_tp = True
             else:
                 tp = position.tp
         else:
-            atr_value = current.atr * atr_multiplier
+            atr_value = current.atr * atr_factor
             if atr_value < min_value:
                 atr_value = min_value
                 logger.warning(f"ATR value adjusted to {atr_value} for {symbol}")
             sl = tick.bid + atr_value
-            assert sl < position.sl, f"current_sl={position.sl} <than new {sl=} in short"
+            assert sl < min(position.sl, position.price_open), f"current_sl={position.sl} <than new {sl=} in short"
             if (position.profit / expected_profit) > extend_profit:
-                tp = position.tp - current.atr * atr_multiplier
+                profit_extension = 1 - extend_profit
+                full_points = abs(position.price_open - position.tp) / symbol.point
+                profit_extension = round(profit_extension * full_points * symbol.point, symbol.digits)
+                tp = position.tp + profit_extension
                 change_tp = True
             else:
                 tp = position.tp
@@ -79,9 +86,9 @@ async def modify_stops(*, position: TradePosition, order: dict, extra: float = 0
         else:
             logger.error(f"Unable to place order due to {res.comment} for {position.symbol}:{position.ticket}")
     except AssertionError as err:
-        logger.error(f"Trailing profits failed due to {err} for {position.symbol}:{position.ticket}: AssertionError")
+        logger.error(f"atr_trailer failed due to {err} for {position.symbol}:{position.ticket}: AssertionError")
     except Exception as err:
-        logger.error(f"Trailing profits failed due to {err} for {position.symbol}:{position.ticket}")
+        logger.error(f"atr_trailer failed due to {err} for {position.symbol}:{position.ticket}")
 
 
 async def send_order(position: TradePosition, sl: float, tp: float) -> OrderSendResult:
