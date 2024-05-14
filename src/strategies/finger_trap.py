8515a57f831b90ffc0b76c9b851a6aeaab533132
Ichinga Samuel <46368656+Ichinga-Samuel@users.noreply.github.com>
2024-05-14 14:24:52 +0100

    update finger_trap.py

--- src/strategies/finger_trap.py
+++ src/strategies/finger_trap.py
@@ -16,24 +16,24 @@ class FingerTrap(Strategy):
     entry_ema: int
     ecc: int
     tcc: int
-    interval: TimeFrame = TimeFrame.M3
+    trend_interval: TimeFrame = TimeFrame.M5
     entry_interval: TimeFrame = TimeFrame.M2
     trader: Trader
     tracker: Tracker
 
     parameters = {"fast_ema": 8, "slow_ema": 21, "etf": TimeFrame.M5, 'closer': ema_closer,
-                  "ttf": TimeFrame.H1, "entry_ema": 5, "tcc": 720, "ecc": 4032}  # 1
+                  "ttf": TimeFrame.H1, "entry_ema": 5, "tcc": 720, "ecc": 4032}  
 
     def __init__(self, *, symbol: ForexSymbol, params: dict | None = None, trader: Trader = None,
                  sessions: Sessions = None, name: str = 'FingerTrap'):
         super().__init__(symbol=symbol, params=params, sessions=sessions, name=name)
         self.trader = trader or BTrader(symbol=self.symbol)
-        self.tracker: Tracker = Tracker(snooze=self.interval.time)  # 2
+        self.tracker: Tracker = Tracker(snooze=self.trend_interval.time)  
 
     async def check_trend(self):
         try:
-            candles: Candles = await self.symbol.copy_rates_from_pos(timeframe=self.ttf, count=self.tcc)  # 3
-            if not ((current := candles[-1].time) >= self.tracker.trend_time):  # 4
+            candles: Candles = await self.symbol.copy_rates_from_pos(timeframe=self.ttf, count=self.tcc) 
+            if not ((current := candles[-1].time) >= self.tracker.trend_time):  
                 self.tracker.update(new=False, order_type=None)
                 return
             self.tracker.update(new=True, trend_time=current, order_type=None)
@@ -41,41 +41,44 @@ class FingerTrap(Strategy):
             candles.ta.ema(length=self.fast_ema, append=True, fillna=0)
             candles.ta.adx(append=True)
             candles.rename(inplace=True, **{f"EMA_{self.fast_ema}": "fast", f"EMA_{self.slow_ema}": "slow",
-                                            "ADX_14": "adx"})  # 5
+                                            "ADX_14": "adx", "DMP_14": "dmp", "DMN_14": "dmn"})  
 
             candles['caf'] = candles.ta_lib.above(candles.close, candles.fast)
             candles['fas'] = candles.ta_lib.above(candles.fast, candles.slow)
 
             candles['fbs'] = candles.ta_lib.below(candles.fast, candles.slow)
-            candles['cbf'] = candles.ta_lib.below(candles.close, candles.fast)  # 6
+            candles['cbf'] = candles.ta_lib.below(candles.close, candles.fast)  
             current = candles[-1]
-            if current.is_bullish() and current.fas and current.caf and current.adx > 25:
+            if current.is_bullish() and current.fas and current.caf and current.adx > 25 and current.dmp > current.dmn:
                 self.tracker.update(trend="bullish")
-            elif current.is_bearish() and current.fbs and current.cbf:
-                self.tracker.update(trend="bearish")  # 7
+            elif current.is_bearish() and current.fbs and current.cbf and current.dmn > current.dmp:
+                self.tracker.update(trend="bearish") 
             else:
-                self.tracker.update(trend="ranging", snooze=self.interval.time, order_type=None)  # 8
+                self.tracker.update(trend="ranging", snooze=self.trend_interval.time, order_type=None)  
         except Exception as err:
             logger.error(f"{err} for {self.symbol} in {self.__class__.__name__}.check_trend")
-            self.tracker.update(snooze=self.interval.time, order_type=None)
+            self.tracker.update(snooze=self.trend_interval.time, order_type=None)
 
     async def confirm_trend(self):
         try:
             candles = await self.symbol.copy_rates_from_pos(timeframe=self.etf, count=self.ecc)
             if not ((current := candles[-1].time) >= self.tracker.entry_time):
-                self.tracker.update(new=False, order_type=None)  # 9
+                self.tracker.update(new=False, order_type=None)  
                 return
             self.tracker.update(new=True, entry_time=current, order_type=None)
             candles.ta.ema(length=self.entry_ema, append=True)
-            candles.rename(**{f"EMA_{self.entry_ema}": "ema"})
-            candles['cae'] = candles.ta_lib.cross(candles.close, candles.ema)
-            candles['cbe'] = candles.ta_lib.cross(candles.close, candles.ema, above=False)  # 10
+            candles.ta.adx(length=3)
+            candles.rename(**{f"EMA_{self.entry_ema}": "ema", "ADX_3": "adx", "DMP_3": "dmp", "DMN_3": "dmn"})
+            candles['cae'] = candles.ta_lib.above(candles.close, candles.ema)
+            candles['cbe'] = candles.ta_lib.below(candles.close, candles.ema)
+            candels['pxn'] = candles.ta_lib.cross(candles.dmp, candles.dmn)
+            candles['nxp'] = candles.ta_lib.cross(candles.dmn, candles.dmp)
             current = candles[-1]
 
-            if self.tracker.bullish and current.cae:
-                self.tracker.update(snooze=self.interval.time, order_type=OrderType.BUY)
-            elif self.tracker.bearish and current.cbe:
-                self.tracker.update(snooze=self.interval.time, order_type=OrderType.SELL)  # 11
+            if self.tracker.bullish and current.cae and current.pxn and current.is_bullish() and current.adx > 25:
+                self.tracker.update(snooze=self.trend_interval.time, order_type=OrderType.BUY)
+            elif self.tracker.bearish and current.cbe and current.nxp and current.is_bearish() and current.adx > 25:
+                self.tracker.update(snooze=self.trend_interval.time, order_type=OrderType.SELL) 
             else:
                 self.tracker.update(snooze=self.entry_interval.time, order_type=None)
         except Exception as err:
@@ -90,7 +93,7 @@ class FingerTrap(Strategy):
     async def trade(self):
         print(f"Trading {self.symbol} with {self.name}")
         async with self.sessions as sess:
-            # await self.sleep(self.interval.time)
+            await self.sleep(self.trend_interval.time)
             while True:
                 await sess.check()
                 try:
